9b933714f1bf8423799863f79f9b795c1251f9b9
AkashSdet13 <akashump1302@gmail.com>
2026-09-16 12:59:56 +0530

    Added the With function example

---
+++ javascript-training/Withfunction.js
@@ -0,0 +1,26 @@
+//1 function==> function is represent a block of code designed to perform task
+
+//how to create function in javascript ot Typescript
+//1.Identify the duplicate code and separate the same from actual test scripts.
+//2.create a block and copy those duplicate steps inside the block
+//3. Give a valid and meaningful name to that particular block to call them easily whenever it's required
+
+//=================================================================
+
+ function loginIntoApplication(browserName){
+//Luunch the chrome browser.
+//enter url "www.icic.com"and launch the application
+//enter username "Akash12"
+//enter password "AKasj"
+//click on the login btn
+
+
+}
+
+function logoutAndBrowser(){
+
+//logout from the application
+//close the browser
+
+}
+
