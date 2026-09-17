23d8fd7db06bcef23857e520ac46fb3f4032f96d
AkashSdet13 <akashump1302@gmail.com>
2026-09-17 15:40:27 +0530

    add the assigment2 for conditional statement

---
+++ typescript-training/assignment/assignment2-coSta.ts
@@ -0,0 +1,67 @@
+// Assignment-3: Conditional Statements
+
+function checkLoanEligibility(
+    customerName: string,
+    creditScore: number,
+    income: number,
+    isEmployed: boolean,
+    debtToIncomeRatio: number
+): void {
+
+    // Credit Score Check
+    if (creditScore > 750) {
+
+        console.log(customerName + " - Loan is automatically approved.");
+
+    } else if (creditScore >= 650 && creditScore <= 750) {
+
+        // Income Check
+        if (income < 50000) {
+
+            console.log(customerName + " - Loan is denied because income is below $50,000.");
+
+        } else {
+
+            // Employment Status Check
+            if (!isEmployed) {
+
+                console.log(customerName + " - Loan is denied because the customer is unemployed.");
+
+            } else {
+
+                // DTI Ratio Check
+                if (debtToIncomeRatio < 40) {
+
+                    console.log(customerName + " - Loan is approved.");
+
+                } else {
+
+                    console.log(customerName + " - Loan is denied because DTI ratio is 40% or greater.");
+
+                }
+            }
+        }
+
+    } else {
+
+        console.log(customerName + " - Loan is denied because credit score is below 650.");
+    }
+}
+
+
+// Customer Details
+let customerName: string = "Karan art";
+let creditScore: number = 720;
+let income: number = 55000.0;
+let isEmployed: boolean = true;
+let debtToIncomeRatio: number = 35.0;
+
+
+// Function Call
+checkLoanEligibility(
+    customerName,
+    creditScore,
+    income,
+    isEmployed,
+    debtToIncomeRatio
+);
