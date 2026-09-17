1132d54964b1d9c0fd76deba92b09bafaddbade0
AkashSdet13 <akashump1302@gmail.com>
2026-09-17 06:27:32 +0530

    data type of Typescript

---
+++ typescript-training/assignment/assignment-data-type.ts
@@ -0,0 +1,70 @@
+//Assignment 1-data types
+
+
+
+let cityTemperature: number = 25.5;
+console.log("City Temperature:" +cityTemperature);
+
+let customerhasplacedanorder: boolean = true;
+console.log("Customer has placed an order:"+customerhasplacedanorder)
+
+let CphoneNumber: number = 8767887267;
+console.log("Customer Phone Number:"+CphoneNumber)
+
+let CbankAcBe: number = 200000300;
+console.log("Customer Account bank Balance:"+CbankAcBe)
+
+let Cemail: string = "cust13@gmail.com";
+console.log("Customer email id:"+Cemail)
+
+
+
+let latitude: number = 37.7749;
+let longitude: number = -122.4194;
+console.log("Co-ordinate location (latitude, longitude): " + latitude + ", " + longitude);
+
+let isMarried: boolean = true;
+console.log("Person is Married status:"+isMarried)
+
+let PjobTitle: string = "SDET";
+console.log("Person job Title:"+PjobTitle)
+
+let FavColor: string = "Yellow";
+console.log("Person Fav Color:"+FavColor)
+
+let CurrentYear: number = 2026;
+console.log("Current Year:"+CurrentYear)
+
+let SocialFllowerCount: string = "600k";
+console.log("Social Media Follower:"+SocialFllowerCount)
+
+let RatingOfMovie: number = 4.9;
+console.log("Rating of Movies:"+RatingOfMovie)
+
+let PersonBloodGroup: string = "A+";
+console.log("Person Blood group:"+PersonBloodGroup)
+
+let TitleOfBook: string = "Winner";
+console.log("Title of book:"+TitleOfBook)
+
+let NoOfEmp: number = 801;
+console.log("Number of Employees:"+NoOfEmp)
+
+let TimeOfEvent: string = "12PM";
+console.log("Time of Event:"+TimeOfEvent)
+
+let CountryName: string = "India";
+console.log("Country Name:"+CountryName)
+
+let EyeColor: string = "Black";
+console.log("Person Eye Color:"+EyeColor)
+
+let birthPlace: string = "Pune";
+console.log("Person Birth place:"+birthPlace)
+
+let DisOf2Cities: string = "28km";
+console.log("Distance of two cities:"+DisOf2Cities)
+
+
+
+
