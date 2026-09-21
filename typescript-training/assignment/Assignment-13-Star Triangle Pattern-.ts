1d28de2da607e76975b0ddde87828437cfd4ddbb
AkashSdet13 <akashump1302@gmail.com>
2026-09-21 17:21:03 +0530

    Star Triangle pattern

---
+++ typescript-training/assignment/Assignment-13-Star Triangle Pattern-.ts
@@ -0,0 +1,12 @@
+let rows: number = 5;
+
+for (let i = 1; i <= rows; i++) {
+
+    let pattern: string = "";
+
+    for (let j = 1; j <= i; j++) {
+        pattern = pattern + "*";
+    }
+
+    console.log(pattern);
+}
